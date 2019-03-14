bfc5e5d3862115521585abd4ec74a39df8f8010f
Alessandro Sebastianelli <alessandroseba1995@gmail.com>
2019-03-14 13:57:13 +0100

    Simulink

---
+++ Simulink/RGB/Parameters.m
@@ -0,0 +1,16 @@
+
+%% This script is executed when the simulink play button is pressed
+
+PWM_AMPLITUDE = 125;
+PHASE_DELAY_R = 0;
+PHASE_DELAY_G = 120;
+PHASE_DELAY_B = 240;
+
+SINE_SAMPLE_TIME = 0.1;
+SAMPLE_PER_PERIOD = 100;
+
+RED_PIN = 3;
+GREEN_PIN = 5;
+BLUE_PIN = 6;
+
+
