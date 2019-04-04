2904240bd130e346fb2bfffc47ffab41c7e9c9c6
Alessandro Sebastianelli <alessandroseba1995@gmail.com>
2019-04-04 13:26:10 +0200

    Identification

---
+++ Simulink/RC_filter/C_identification/C_id.m
@@ -0,0 +1,28 @@
+%% Load data
+load('test_002.mat');
+in = timeseries(voltage_resistor, time);
+out_1 = timeseries(voltage_capacitor, time);
+
+%% Parameters
+R = 10.09*10^3;
+C_vec = 90e-6:1e-6:110e-6;
+
+%% Simulink model
+model = 'C_identification';
+
+for i=1:length(C_vec)
+    C = C_vec(i);
+    tau = R*C;
+    
+    %% Start simulation
+    sim(model);
+    sim_out(i,:) = square_error;
+    plot(square_error, 'LineWidth', 2); hold on;
+end
+
+to_min = sim_out(:,end);
+
+[m, i] = min(to_min);
+display('Capacitor value')
+C = C_vec(i)
+tau = R*C;
--- Simulink/RC_filter/Simulation.m
+++ Simulink/RC_filter/Simulation.m
@@ -7,17 +7,21 @@
 figure;
 voltage_capacitor_s = timeseries(voltage_capacitor, time);
 voltage_resistor_s = timeseries(voltage_resistor, time);
+
 plot(voltage_capacitor_s, 'LineWidth', 2); hold on;
 plot(voltage_resistor_s, 'LineWidth', 2); hold on;
 title("RC Filter (Real system)", 'FontSize', 20);
 xlabel("Time [s]", 'FontSize', 16);
+xlim([0 100]);
 ylabel("Voltage [V]", 'FontSize', 16);
-legend({"Capacitor", "Resistor"}, 'FontSize', 16);
+legend({"Capacitor", "V_{in}"}, 'FontSize', 16);
 
 figure;
 plot(voltage_capacitor_s, 'LineWidth', 2); hold on;
 plot(voltage_capacitor_sim, 'LineWidth', 2); hold on;
+plot(voltage_resistor_s, 'LineWidth', 2); hold on;
 title("RC Filter (Real system VS Simulation)", 'FontSize', 20);
+xlim([0 100]);
 xlabel("Time [s]", 'FontSize', 16);
 ylabel("Voltage [V]", 'FontSize', 16);
-legend({"Real", "Simulation"}, 'FontSize', 16);
+legend({"Real", "Simulation", "V_{in}"}, 'FontSize', 16);
